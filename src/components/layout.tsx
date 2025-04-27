b984cd9c00446d7842f2dacf22ff4d415ffbcbd5
Saurabhsaxena81 <saurabhsaxena81272@gmail.com>
2025-04-27 16:43:57 +0530

    First commit

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,7 +1,11 @@
 import type { PropsWithChildren } from "react";
 import { Header } from "./header";
+import { text } from "stream/consumers";
+
+import { useTheme } from "@/context/theme-provider";
 
 export function Layout({ children }: PropsWithChildren) {
+    const { theme } = useTheme();
   return (
     <div className=" bg-gradient-to-br from-background to-muted">
       <Header />
@@ -9,7 +13,7 @@ export function Layout({ children }: PropsWithChildren) {
         {children}
       </main>
       <footer className="border-t backdrop-blur supports-[backdrop-filter]:bg-background/60 py-12">
-        <div className="container mx-auto px-4 text-center text-gray-200">
+      <div className={`container mx-auto px-4 text-center ${theme === 'dark' ? 'text-gray-200' : 'text-gray-700'}`}>
           <p>Made with 💗 by Saurabh Saxena</p>
         </div>
       </footer>
